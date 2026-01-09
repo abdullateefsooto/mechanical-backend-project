2ce929840cba8f2f37ed30b488090ea91a342bd4
abdullateef <abdullateefopeyemi06@gmail.com>
2026-01-09 16:51:29 +0100

    Fix backend for Render deployment

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 
 
