084157f41e28d4ce823e05bcb380716e8108ea9e
abdullateef <abdullateefopeyemi06@gmail.com>
2026-01-11 18:30:29 +0100

    fixing 404 err

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.post("/contact", async (req, res) => {
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("/.*/", (req, res) => {
       res.sendFile(
         path.join(__dirname, "frontend/dist", "index.html")
       );
