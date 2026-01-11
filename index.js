af505f9a2130c9bec0b7a22e9f98a9fe7d6d4a4c
abdullateef <abdullateefopeyemi06@gmail.com>
2026-01-11 18:26:04 +0100

    fixing 404 err

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 import path from 'path';
+import { fileURLToPath } from "url";
 
+
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 
