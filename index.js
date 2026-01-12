34205e49012c14e2a0dbda7b426d412da74d94c9
abdullateef <abdullateefopeyemi06@gmail.com>
2026-01-12 10:57:23 +0100

    fixing 404 er

--- index.js
+++ index.js
@@ -106,18 +106,6 @@ app.post("/contact", async (req, res) => {
   }
 });
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-    app.get("/.*", (req, res) => {
-      res.sendFile(
-        path.join(__dirname, "frontend/dist", "index.html")
-      );
-    });
-  }
-
-
-
 
 
 app.listen(PORT, () => {
